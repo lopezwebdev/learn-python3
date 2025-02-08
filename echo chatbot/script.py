8d4c82bfd5d438c7760a63dbdaf6f87038ce7825
Esteban Lopez 👋 <esteban.steam21@gmail.com>
2025-02-08 12:03:04 +0900

    Create script.py

---
+++ echo chatbot/script.py
@@ -0,0 +1,8 @@
+print("Welcome to Esteban's echo chatbot!")
+print("I will repeat after you.")
+
+stuff_to_ech = input("Enter something for me to repeat: Hello echo chatbot! ")
+
+print("You said: " + stuff_to_echo)
+
+
